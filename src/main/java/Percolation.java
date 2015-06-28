6b36f422e4d67176bc43a6ef8c34f4c870f842cd
Ivan <saintjoint@gmail.com>
2015-06-28 16:25:06 +0300

    initial  implementation of Percolation#open() function

--- src/main/java/Percolation.java
+++ src/main/java/Percolation.java
@@ -44,9 +44,10 @@
 public class Percolation {
 
     private int flatSitesGrid[];
-    private int componentsSize[];
+    private int componentWeights[];
     private int countOfComponents;
     private int sitesGridSize;
+    private int gridDimension;
     private final int COUNT_OF_VIRTUAL_SITES = 2;
     private final byte SITE_BLOCKED_STATE = -1;
 
@@ -56,75 +57,99 @@ public class Percolation {
             throw new IllegalArgumentException("wrong size of grid");
         }
 
-        sitesGridSize = N * N + COUNT_OF_VIRTUAL_SITES;
-        countOfComponents = N * N;
+        gridDimension = N;
+        countOfComponents = gridDimension * gridDimension;
+        sitesGridSize = countOfComponents + COUNT_OF_VIRTUAL_SITES;
+
         flatSitesGrid = new int[sitesGridSize];
-        componentsSize = new int[sitesGridSize];
+        componentWeights = new int[sitesGridSize];
 
         /*
         mark very first site, and a last one  as a virtual top and bottom sites,
          which are open (have reference to a root) by default
          */
-        // flatSitesGrid[0] = 0; --> initialized with zero by default, so the operation is redundant
+        flatSitesGrid[0] = 0; // in fact, it's initialized with zero by default, so the operation is redundant
         flatSitesGrid[sitesGridSize - 1] = sitesGridSize - 1;
 
         /*
          assign default size  ("1") for virtual site components.
-         all other components will get "0" while initialization of componentsSize array by default
+         all other components will get "0" while initialization of componentWeights array by default
          */
-        componentsSize[0] = 1;
-        componentsSize[sitesGridSize - 1] = 1;
+        componentWeights[0] = 1;
+        componentWeights[sitesGridSize - 1] = 1;
 
-        //initialize grid with blocked sites
-        for (int i = 1; i < sitesGridSize - 1; i++) {
+        //initialize grid with blocked sites, despite virtual  sites
+        for (int i = 1; i < sitesGridSize - 2; i++) {
             flatSitesGrid[i] = SITE_BLOCKED_STATE;
         }
-
     }
 
     // open site (row i, column j) if it is not open already
     public void open(int i, int j) {
         if (!isOpen(i, j)) {
 
+            int currentSiteIndex = (i - 1) * gridDimension + j;
+
+            //look to bottom
+            boolean isBottomVirtualSite = ((i + 1) > gridDimension);
+            int bottomSiteIndex;
+            if (!isBottomVirtualSite) {
+                bottomSiteIndex = sitesGridSize + 1;
+                //union (currentSiteIndex, bottomSiteIndex]
+            }else if(isOpen(i + 1, j)){
+                bottomSiteIndex = i * gridDimension + j;
+                //union (currentSiteIndex, bottomSiteIndex]
+            }
+
+            //look up
+            boolean isTopVirtualSite = ((i - 1) == 0);
+            int topSiteIndex;
+            if (isTopVirtualSite) {
+                topSiteIndex = 0;
+                //union (currentSiteIndex, topSiteIndex)
+            } else if (isOpen(i - 1, j)) {
+                topSiteIndex = (i - 2) * gridDimension + j;
+                //union (currentSiteIndex, topSiteIndex)
+            }
+
+            //look to the left
+            if ((j - 1 > 0) && isOpen(i, j - 1)) {
+                int leftSiteIndex = (i - 1) * gridDimension + (j - 1);
+                //union (currentSiteIndex, leftSiteIndex)
+            }
+
+            //look to the right
+            if ((j + 1 <= gridDimension) && isOpen(i, j + 1)) {
+                int rightSiteIndex = (i - 1) * gridDimension + (j + 1);
+                //union (currentSiteIndex, rightSiteIndex)
+            }
+
+            //open site with link to itself in case has no siblings
+            if(!isOpen(i, j)){
+                flatSitesGrid[currentSiteIndex] = currentSiteIndex;
+            }
+
         }
     }
 
     // is site (row i, column j) open?
     public boolean isOpen(int i, int j) {
-        if (((i < 1 || i > Math.sqrt(sitesGridSize)) && (j < 1 || j > Math.sqrt(sitesGridSize)))) {
-            throw new IndexOutOfBoundsException("indexes are out ouf rande : 1.." + Math.sqrt(sitesGridSize));
+        if (((i < 1 || i > gridDimension) && (j < 1 || j > gridDimension))) {
+            throw new IndexOutOfBoundsException("indexes are out ouf rande : 1.." + gridDimension);
         }
-        int index = i * j;
-        return flatSitesGrid[index] != SITE_BLOCKED_STATE;
+
+        return flatSitesGrid[(i - 1) * gridDimension + j] != SITE_BLOCKED_STATE;
     }
 
     // is site (row i, column j) full?
     public boolean isFull(int i, int j) {
-        boolean upSibling = hasUpSibling(i, j);
-        boolean bottomSibling = hasBottomSibling(i, j);
-
-        return isOpen(i, j) && hasBottomSibling(i, j) && hasUpSibling(i, j) && hasLeftSibling(i, j) && hasRightSibling(i, j);
+        return isOpen(i, j) &&
+                hasBottomSibling(i, j) &&
+                hasUpSibling(i, j) &&
+                hasLeftSibling(i, j) &&
+                hasRightSibling(i, j);
     }
 
-    private boolean isEdge(int index) {
-        return ((index % Math.sqrt((double) sitesGridSize))) == 0;
-    }
-
-    private boolean hasUpSibling(int i, int j) {
-        return isEdge((i + 1) * j) || isOpen(i + 1, j);
-    }
-
-    private boolean hasBottomSibling(int i, int j) {
-        return isEdge((i - 1) * j) || isOpen(i - 1, j);
-    }
-
-    private boolean hasLeftSibling(int i, int j) {
-        return isEdge(i * (j - 1)) || isOpen(i, j - 1);
-    }
-
-    private boolean hasRightSibling(int i, int j) {
-        return isEdge(i * (j + 1)) || isOpen(i, j + 1);
-    }
 
     // does the system percolate?
     public boolean percolates() {
@@ -134,5 +159,33 @@ public class Percolation {
     // test client (optional)
     public static void main(String[] args) {
 
+    }
+
+    private boolean isOutOfBound(int index) {
+        return (index % gridDimension) == 0;
+    }
+
+    private boolean hasUpSibling(int i, int j) {
+        return isOutOfBound((i - 1) * j) || isOpen(i - 1, j);
+    }
+
+    private boolean hasBottomSibling(int i, int j) {
+        return isOutOfBound((i + 1) * j) || isOpen(i + 1, j);
+    }
+
+    private boolean hasLeftSibling(int i, int j) {
+        return isOutOfBound(i * (j - 1)) || isOpen(i, j - 1);
+    }
+
+    private boolean hasRightSibling(int i, int j) {
+        return isOutOfBound(i * (j + 1)) || isOpen(i, j + 1);
+    }
+
+    private void weightAndUnion(int p, int k){
+
+    }
+
+    private boolean find(int p, int k){
+        return false;
     }
 }
